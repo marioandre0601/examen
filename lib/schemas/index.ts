de19d64da45898ef24ff18bc31fa9cf0245a6bd0
marioandre0601 <marioandre.06.01@gmail.com>
2021-10-18 12:57:05 -0500

    swagger and translate schema

--- lib/schemas/index.ts
+++ lib/schemas/index.ts
@@ -7,10 +7,10 @@ export const customer_schema: JsonSchema = {
 		email: {
 			description: 'Primary key',
 			type: JsonSchemaType.STRING
-		},
+		},/** email -> correo_electronico */
 		document_number: {
 			type: JsonSchemaType.STRING
-		},
+		},/** document_number -> numero_de_documento */
 		document_type: {
 			type: JsonSchemaType.STRING,
 			enum: [
@@ -18,13 +18,13 @@ export const customer_schema: JsonSchema = {
 				'RUC',
 				'CE',
 			]
-		},
+		},/** document_type -> tipo_de_documento */
 		name: {
 			type: JsonSchemaType.STRING
-		},
+		},/** name -> nombre */
 		last_name: {
 			type: JsonSchemaType.STRING
-		},
+		},/** last_name -> apellido */
 	},
 	additionalProperties: false,
 	required: [
